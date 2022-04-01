8578dbe08c8fbcaca26360694bc97d51280c2da6
Jay51982 <jay_51982@yahoo.co.uk>
2022-04-01 01:15:24 +0100

    My homework week 7 -repeat

---
+++ src/Program_6_OddEven.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Program_6_OddEven {
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want to check : ");
+        int number = scanner.nextInt();
+        Program_6_OddEven oddEven = new Program_6_OddEven();
+        System.out.println(number + " is " + oddEven.isItEvenOrOddNumber(number) + " number");
+        // Closing the scanner object
+        scanner.close();
+    }
+
+    // Checking the odd or even
+    public String isItEvenOrOddNumber(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+}
---
+++ src/Program_8_PrintCityName.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Input any alphabet from “A to “F” and print their city name accordingly (use if else) if
+ * any other alphabet should be invalid entry
+ */
+
+public class Program_8_PrintCityName {
+    public static void main(String[] args) {
+        // Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter alphabet between A to F : ");
+        char city = scanner.next().charAt(0);
+        //Object creation
+        Program_8_PrintCityName cityName = new Program_8_PrintCityName();
+        cityName.printCityName(city);
+        //Closing the scanner object
+        scanner.close();
+    }
+//pirnting city name
+
+    public void printCityName(char city){
+        if (city == 'A' || city == 'a') {
+            System.out.println("Aberdeen");
+        } else if (city == 'B' || city == 'b') {
+            System.out.println("Belfast");
+        } else if (city == 'C' || city == 'c') {
+            System.out.println("Cambride");
+        } else if (city == 'E' || city == 'e') {
+            System.out.println("Edinburgh");
+        } else if (city == 'F' || city == 'f') {
+            System.out.println("Feltham");
+        } else {
+            System.out.println("The aplhabet your enter is not between A to F");
+        }
+
+    }
+}
